4450b5cd0c9182530c4990dbd9b05e57bdef7220
unknown <viniciusbeloti7@gmail.com>
2020-08-27 14:06:13 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,5 +1,6 @@
 import { MongoClient, Collection } from 'mongodb';
 import { disconnect } from 'process';
+import { AccountModel } from '../../../../domain/models/account';
 
 export const MongoHelper = {
   client: null as MongoClient,
@@ -16,5 +17,10 @@ export const MongoHelper = {
 
   getCollection (name: string): Collection {
     return this.client.db().collection(name);
+  },
+
+  map (collection: any): any {
+    const { _id, ...collectionWithoutId } = collection;
+    return Object.assign({}, collectionWithoutId, { id: _id });
   }
 };
