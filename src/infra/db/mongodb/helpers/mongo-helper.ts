6c5dbb2d056cc602cb2dac47e11790acd5deb6b8
unknown <viniciusbeloti7@gmail.com>
2020-08-27 13:35:45 -0300

    refactor: move mongodb mehotds to a helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb';
+import { disconnect } from 'process';
+
+export const MongoHelper = {
+  client: null as MongoClient,
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close();
+  }
+};
