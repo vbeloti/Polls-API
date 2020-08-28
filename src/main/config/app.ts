63498fb75c42c837fa7e1aa9712cce0222e02a69
unknown <viniciusbeloti7@gmail.com>
2020-08-28 09:14:43 -0300

    feat: esnrue SignUp route returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import setupMiddlerares from './middlewares';
+import setupRoutes from './routes';
 
 const app = express();
 setupMiddlerares(app);
-
+setupRoutes(app);
 export default app;
