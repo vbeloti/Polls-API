bffc47f6b672a16404ecf13d9787b487cc70b52c
unknown <viniciusbeloti7@gmail.com>
2020-09-05 10:43:33 -0300

    refactor: rename signup routes with login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -3,7 +3,7 @@ import app from '../config/app';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 import env from '../config/env';
 
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(env.mongoUrl);
   });
@@ -17,15 +17,17 @@ describe('SignUp Routes', () => {
     await accountCollection.deleteMany({});
   });
 
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Vinicius',
-        email: 'vinicius.beloti@gmail.com',
-        password: '123',
-        passwordConfirmation: '123'
-      })
-      .expect(200);
+  describe('POST /signup', () => {
+    test('Should return 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Vinicius',
+          email: 'vinicius.beloti@gmail.com',
+          password: '123',
+          passwordConfirmation: '123'
+        })
+        .expect(200);
+    });
   });
 });
