2bbf2ebf6668f9a8ae5430aad214a154b13ec23b
unknown <viniciusbeloti7@gmail.com>
2020-09-08 15:37:07 -0300

    chore: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,6 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/polls-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://mongo:27017/polls-api',
+  // mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/polls-api',
   port: process.env.PORT || 3333,
   jwtSecret: process.env.JWT_SECRET || 'tj61s5saqa=-1as'
 };
