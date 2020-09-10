98cd9125b27faa5f0b0e044e87bdc4f87066339f
unknown <viniciusbeloti7@gmail.com>
2020-09-10 11:33:21 -0300

    refactor: move validation to its own layer

--- src/validation/validators/email-validation.spec.ts
+++ src/validation/validators/email-validation.spec.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { EmailValidation } from './email-validation';
-import { EmailValidator } from '../../protocols/email-validator';
-import { InvalidParamError } from '../../errors';
+import { EmailValidator } from '../protocols/email-validator';
+import { InvalidParamError } from '../../presentation/errors';
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
