7bdd0aae6f03f57f0554b713af1e5a0f999b1abd
unknown <viniciusbeloti7@gmail.com>
2020-09-17 13:42:03 -0300

    test: ensure DbLoadSurveys returns a list of surveys on success

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -54,4 +54,10 @@ describe('DbLoadSurveys', () => {
 
     expect(loadAllSpy).toHaveBeenCalled();
   });
+
+  test('Should return a list of Surveys on success', async () => {
+    const { sut } = makeSut();
+    const surveys = await sut.load();
+    expect(surveys).toEqual(makeFakeSurveys());
+  });
 });
