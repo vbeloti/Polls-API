ee368eb7bedd06011399fa323eadf5d3aa187f67
unknown <viniciusbeloti7@gmail.com>
2020-09-17 11:30:36 -0300

    refactor: remove protocols to coverage

---
+++ README.md
@@ -0,0 +1,20 @@
+[![Build Status](https://travis-ci.org/vbeloti/Polls-API.svg?branch=master)](https://travis-ci.org/vbeloti/Polls-API)
+
+## Polls Api (Node.js && Typescript && Express.js && Postgres)
+
+
+# Polls Api
+
+## Open Endpoints
+
+Open endpoints require no Authentication.
+
+* [Signup]: `POST /api/signup` -- Create Account
+
+## Closed Endpoints
+
+Closed endpoints required Authentication.
+
+* [Signin]: `POST /api/signin` -- Login Account
+* [Pools]: `POST /api/surveys` -- Create survey
+* [Pools]: `GET /api/surveys` -- List All surveys
--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,20 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/**/*-protocols-controller.ts',
+    '!**/protocols/**',
+    '!**/test/**'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
   }
-};
+}
