b09e297742c143629f36389f1a2598b7b1c3e08c
unknown <viniciusbeloti7@gmail.com>
2020-10-06 11:28:48 -0300

    feat: add swagger api

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import setupMiddlerares from './middlewares';
+import setupSwagger from './config-swagger';
 import setupRoutes from './routes';
 
 const app = express();
+setupSwagger(app);
 setupMiddlerares(app);
 setupRoutes(app);
 export default app;
---
+++ src/main/docs/index.ts
@@ -0,0 +1,13 @@
+export default {
+  openapi: '3.0.0',
+  info: {
+    title: 'Surveys Api',
+    description: 'Essa é a documentação da API Surveys Api',
+    version: '1.0.0',
+    contact: {
+      name: 'Vinicius Beloti',
+      email: 'vinicius.beloti@gmail.com',
+      url: 'https://www.linkedin.com/in/vinicius-beloti/'
+    }
+  }
+};
