3224c63f04e89850e236b6b2c6d026ac3e3d3ff1
unknown <viniciusbeloti7@gmail.com>
2020-10-06 11:52:36 -0300

    refactor: separate docs files

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,3 +1,7 @@
+import paths from './paths';
+import components from './components';
+import schemas from './schemas';
+
 export default {
   openapi: '3.0.0',
   info: {
@@ -8,6 +12,24 @@ export default {
       name: 'Vinicius Beloti',
       email: 'vinicius.beloti@gmail.com',
       url: 'https://www.linkedin.com/in/vinicius-beloti/'
+    },
+    license: {
+      name: 'GPL-3.0-or-later',
+      url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
     }
-  }
+  },
+  servers: [{
+    url: '/api',
+    description: 'Servidor Principal'
+  }],
+  tags: [{
+    name: 'Login',
+    description: 'APIs relacionadas a Login'
+  }, {
+    name: 'Enquete',
+    description: 'APIs relacionadas a Enquete'
+  }],
+  paths,
+  schemas,
+  components
 };
